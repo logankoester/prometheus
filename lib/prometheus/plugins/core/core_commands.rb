4569a348cb47165bdf1281b3478bf9569f39edf1
Logan Koester <logan@logankoester.com>
2012-04-19 14:41:55 -0400

    Sets superclass template path

--- lib/prometheus/plugins/core/core_commands.rb
+++ lib/prometheus/plugins/core/core_commands.rb
@@ -1,7 +1,7 @@
 module Prometheus
   class Commands < Prometheus::Base
     include Thor::Actions
-    source_root TEMPLATES_PATH
+    source_root File.expand_path('../../../../templates', __FILE__)
     # Tasks added here will be available in all generated Prometheus applications.
   end
 end
